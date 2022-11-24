bbd7fdcd05fd4092ac58e6a56aee733986c27f0c
Герасимов Максим Николаевич <gerasimmaks@mail.ru>
2022-11-24 18:36:45 +0300

    задача решена

---
+++ Seminar 1.0/task 1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите число 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2)
+{
+Console.WriteLine("да");
+}
+
+else
+{
+Console.WriteLine("нет");
+}
