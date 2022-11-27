d53074a245b244d862516db61aff31a88e8692f4
«Maksim <gerasimmaks@mail.ru>
2022-11-27 23:25:06 +0300

    задача решена

---
+++ Seminar 2.0/task 11/Program.cs
@@ -0,0 +1,14 @@
+﻿// убрать вторую цифру из трехзначного числа
+
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine (number);
+int digit1 = number / 100;
+int digit2 = number % 10;
+
+// Console.WriteLine (digit1);
+// Console.WriteLine (digit2);
+//Console.WriteLine ($"{digit1}{digit2}");
+
+int digit3 = digit1*10 + digit2;
+Console.WriteLine (digit3);
