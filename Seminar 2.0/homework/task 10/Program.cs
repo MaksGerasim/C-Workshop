e945ed8702cb799f0c285d5a979fee42f064e5a6
«Maksim <gerasimmaks@mail.ru>
2022-11-28 23:45:51 +0300

    задача решена

---
+++ Seminar 2.0/homework/task 10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// ​
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+//int number = new Random().Next(100, 1000);
+
+Console.WriteLine("введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digit2 = number % 10;
+int digit1 = number / 100;
+
+int digit3 = (number - digit1*100- digit2)/10;
+
+Console.WriteLine (digit3);
+
+
