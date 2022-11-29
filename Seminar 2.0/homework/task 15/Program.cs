32fb3cddb89b52c7f182d989604a51e9850110ac
«Maksim <gerasimmaks@mail.ru>
2022-11-29 22:06:57 +0300

    задача решена

---
+++ Seminar 2.0/homework/task 15/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// ​
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.WriteLine ("введите число");
+int number = Convert.ToInt32 (Console.ReadLine ());
+
+if (number == 6 || number == 7) Console.WriteLine ("да");
+if (number == 1 || number == 2 || number == 3 || number == 4 || number == 5) Console.WriteLine ("нет");
+if (number < 1 || number > 7) Console.WriteLine ("введите число от 1 до 7");
