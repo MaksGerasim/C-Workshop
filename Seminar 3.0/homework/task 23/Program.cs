fb0a4e23b7f7728f729f4894c26167d8c0d7ba4c
«Maksim <gerasimmaks@mail.ru>
2022-11-30 22:25:46 +0300

    задача решена

---
+++ Seminar 3.0/homework/task 23/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine ("введите число");
+int number = Convert.ToInt32 (Console.ReadLine());
+int count = 1;
+
+if (number < 0)
+{
+    Console.WriteLine ("введите положительное значение");
+}
+else
+while (count < number || count == number)
+      {
+       double square = Math.Pow((count),3);
+       Console.WriteLine (square);
+       count++;
+      }
