244a0572492eb5a4916686368c28849b1e97064d
«Maksim <gerasimmaks@mail.ru>
2022-11-30 21:19:00 +0300

    cохранение 29.11.22

---
+++ Seminar 3.0/task 17/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+Console.WriteLine ("введите x");
+int numX = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine ("введите y");
+int numY = Convert.ToInt32 (Console.ReadLine());
+
+if (numX > 0 && numY > 0)
+{
+    Console.Write("точка в первой четверти");
+}
+else if (numX < 0 && numY > 0)
+{
+    Console.Write("точка во второй четверти");
+}
+else if (numX < 0 && numY < 0)
+{
+    Console.Write("точка в третьей четверти");
+}
+else if (numX > 0 && numY < 0)
+{
+    Console.Write("точка в четвертой четверти");
+}
+else
+{
+    Console.Write("заданное значение неверное");
+}
