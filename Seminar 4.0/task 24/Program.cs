30d687159f9b9fd0432faa8e5e57214c2678790a
«Maksim <gerasimmaks@mail.ru>
2022-12-05 21:06:12 +0300

    задача решена

---
+++ Seminar 4.0/task 24/Program.cs
@@ -0,0 +1,55 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+bool Validate (int number)
+{
+    if (number > 1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int Sum1toA (int A)
+{
+    int sum = 0;
+    for (int i = 1; i <= A; i++)
+    {
+         sum += i;
+    }
+    return sum;
+}
+
+int GetNumber (string message)
+{
+    Console.WriteLine ("введите значение");
+    return Convert.ToInt32 (Console.ReadLine ());
+}
+
+int number = GetNumber("введите число");
+bool isCorrect = Validate(number);
+if (isCorrect == true)
+{
+    int sum = Sum1toA(number);
+    Console.WriteLine ($"Сумма чисел от 1 до {number} = {sum}");
+}
+else
+{Console.WriteLine ("невозможно получить сумму от 1 до {number}");
+}
+
+
+
+
+
+
+
+// Console.WriteLine ("введите значение");
+
+// int number = Convert.ToInt32 (Console.ReadLine());
+
+// Console.WriteLine (number);
