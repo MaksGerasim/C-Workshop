539dc90e5926aa06378ca42303fa31756a280ed7
«Maksim <gerasimmaks@mail.ru>
2022-12-05 23:43:40 +0300

    задача решена

---
+++ Seminar 4.0/task 28/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+int GetNumber (string message)
+{
+    Console.WriteLine ("введите значение");
+    return Convert.ToInt32 (Console.ReadLine ());
+}
+
+
+int multnum1 (int A)
+{
+    int multnum = 1;
+    int count = 1;
+while (count <= A)
+{
+    multnum = multnum * count;
+    count++;
+}
+   return multnum;
+}
+
+int multnum2 (int A)
+{
+    int multnu = 1;
+    for (int i = 1; i <= A; i++)
+    {
+         multnu = multnu * i;
+    }
+    return multnu;
+}
+
+
+int number = GetNumber ("");
+int finishmultnum = multnum2 (number);
+Console.WriteLine (finishmultnum);
+
+
+
+
+
+
+
+
