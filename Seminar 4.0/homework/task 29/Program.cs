d753a9cd59a4c932a7600855be28b2252f5ad800
«Maksim <gerasimmaks@mail.ru>
2022-12-06 22:44:38 +0300

    задача решена

---
+++ Seminar 4.0/homework/task 29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем
+
+
+
+
+
+
+int[] array = new int [8];
+
+for (int i = 0; i < array.Length; i++)
+{
+Console.WriteLine ("введите значение ячейки массива");    
+array [i] = Convert.ToInt32 (Console.ReadLine());
+}
+Console.WriteLine (string.Join(",", array));
