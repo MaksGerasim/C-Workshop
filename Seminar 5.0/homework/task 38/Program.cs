4ddb4defdce3630067482b732bb085623badad6a
«Maksim <gerasimmaks@mail.ru>
2022-12-09 22:32:33 +0300

    задача решена

---
+++ Seminar 5.0/homework/task 38/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте массив натуральных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+
+int[] RandomArray(int size, int leftRange, int rightRange) // создание случайного массива
+{
+    int[] array = new int[size]; 
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+int DifferenceMinMaxNumber (int [] array)
+{
+    int Diff = 0;
+    int MinNumber = array[0];
+    int MaxNumber = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < MinNumber)
+        {
+            MinNumber = array[i];   
+        }
+        else if (array[i] > MaxNumber)
+        {
+            MaxNumber = array[i];
+        }
+    }
+Console.WriteLine ($"минимальное значение = {MinNumber}");
+Console.WriteLine ($"максимальное значение = {MaxNumber}");
+    Diff = MaxNumber - MinNumber;
+    return Diff;
+}
+
+const int SIZE = 5;
+const int LEFTRANGE = 1;
+const int RIGHTRANGE = 99;
+
+int [] RandArray = RandomArray (SIZE, LEFTRANGE, RIGHTRANGE);
+Console.WriteLine(string.Join (",", RandArray));
+int DiffMinMax = DifferenceMinMaxNumber (RandArray);
+Console.WriteLine ($"Difference  = {DiffMinMax}");
