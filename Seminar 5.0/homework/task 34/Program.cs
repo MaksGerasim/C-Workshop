e78d68962ba3193c7e70767bfae84e9536bf8954
«Maksim <gerasimmaks@mail.ru>
2022-12-09 21:29:35 +0300

    задача решена

---
+++ Seminar 5.0/homework/task 34/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int[] RandomArray(int size, int leftRange, int rightRange) // создание случайного массива
+{
+    int[] array = new int[size]; 
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+
+int CountPositiveNumber (int [] array)
+{
+    int CountPosNum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]%2 == 0)
+        {
+            CountPosNum++;
+        }
+    }
+    return CountPosNum;
+}
+
+const int SIZE = 5;
+const int LEFTRANGE = 100;
+const int RIGHTRANGE = 999;
+
+int [] RandArray = RandomArray (SIZE, LEFTRANGE, RIGHTRANGE);
+Console.WriteLine(string.Join (",", RandArray));
+int CountPositive = CountPositiveNumber (RandArray);
+Console.WriteLine ($"количество четных значений = {CountPositive}");
