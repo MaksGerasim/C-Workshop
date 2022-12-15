0dc92cba84a6bd171d00ae90c64b13553ec7d0d8
unknown <gerasimmaks@mail.ru>
2022-12-15 17:48:31 +0300

    задача решена

---
+++ Seminar 6.0/Task 44/Program.cs
@@ -0,0 +1,24 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int [] ArrayFi = new int [number];
+ArrayFi[0] = 0;
+ArrayFi[1] = 1;
+
+for (int i = 2; i < number; i++)
+{
+   ArrayFi[i] = ArrayFi[i-1] + ArrayFi[i-2];
+}
+
+Console.WriteLine(string.Join(",", ArrayFi));
+
+
+
+
+
+
