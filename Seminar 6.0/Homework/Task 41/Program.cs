fc67fc8431496a8204fea7783d716d29ccb898df
unknown <gerasimmaks@mail.ru>
2022-12-17 13:38:17 +0300

    задача решена

---
+++ Seminar 6.0/Homework/Task 41/Program.cs
@@ -0,0 +1,86 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+// больше 0 ввёл пользователь. Ввод чисел останавливается при помощи ввода стоп-слова "stop"
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+
+// int Metod ()
+// {
+// Console.WriteLine("введите значение");
+// string? stopstatus = Console.ReadLine();
+// int number = 0;
+// int count = 0;
+// if (stopstatus != "stop")
+// {
+// number = Convert.ToInt32(stopstatus);
+// // Console.WriteLine(number);
+// Metod();
+// count++;
+// }
+// return number;
+// }
+
+
+void CountNumber ()
+{
+string? stopstatus = "";
+int number = 0;
+int count = 0;
+ while (stopstatus != "stop")
+ {
+    Console.WriteLine("введите значение");
+    stopstatus = Console.ReadLine();
+
+    if (stopstatus != "stop")
+    {
+        number = Convert.ToInt32(stopstatus);
+        if (number > 0)
+        count++;
+    }
+ }
+Console.WriteLine($"количество введенных значений > 0 = {count}");
+}
+
+
+
+CountNumber ();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
