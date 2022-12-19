82701206bfc5582ebe399fc764cd781607966362
unknown <gerasimmaks@mail.ru>
2022-12-19 14:09:00 +0300

    задача решена

---
+++ Seminar 7.0/Task 49/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// ​
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+
+
+int[,] RandomTwoDimensionalArray(int rowsCount, int colunsCount, int leftRange, int rightRange)
+{
+    int[,] array = new int[rowsCount, colunsCount];
+     
+    Random rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return array;
+}
