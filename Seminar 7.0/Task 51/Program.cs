041403eaebc8e2ccae8978934595bb2f20bdffb6
unknown <gerasimmaks@mail.ru>
2022-12-19 16:04:43 +0300

    задача решена

--- Seminar 7.0/Task 51/Program.cs
+++ Seminar 7.0/Task 51/Program.cs
@@ -6,3 +6,57 @@
 // Сумма элементов главной диагонали: 1+9+2 = 12
 // ​
 // Доп. условие для 51 задачи: сделать суммирование в один цикл. Матрица может быть прямоугольный
+
+
+int[,] RandomTwoDimensionalArray(int rowsCount, int colunsCount, int leftRange, int rightRange)
+{
+    int[,] array = new int[rowsCount, colunsCount];
+     
+    Random rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return array;
+}
+
+
+int AddDiagonnalNumbers(int [,] matrix)
+{
+    int AddNum = 0;
+    int j = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        AddNum += matrix[i,j];
+        j++;
+    }
+    return AddNum;
+}
+
+void PrintMatrix (int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr [i,j] + " ");     
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+const int ROWSCOUNT = 4;
+const int COLUNSCOUNT = 4;       
+const int lEFTRANGE = 1;
+const int RIGHTRANGE = 9;
+
+int [,] RandMatrix = RandomTwoDimensionalArray(ROWSCOUNT, COLUNSCOUNT, lEFTRANGE, RIGHTRANGE);
+PrintMatrix(RandMatrix);
+Console.WriteLine();
+int AddDiag = AddDiagonnalNumbers(RandMatrix);
+Console.WriteLine($"сумма элементтов диагонали = {AddDiag}");
