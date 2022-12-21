b63fda406994ce22d57cfceca030c86a3000f809
«Maksim <gerasimmaks@mail.ru>
2022-12-21 22:43:28 +0300

    проработать

---
+++ Seminar 8.0/task 59/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Напишите программу, которая удалит строку и столбец, 
+// на пересечении которых расположен наименьший элемент массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Наименьший элемент - 1, на выходе получим
+// следующий массив:
+// 9 4 2
+// 2 2 6
+// 3 4 7
+
+
+int[,] RandomTwoDimensionalArray(int rowsCount, int colunsCount, int leftRange, int rightRange)
+{
+    int[,] array = new int[rowsCount, colunsCount];
+     
+    Random rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return array;
+}
+
+
+int [,] DeleteRowColMinNumber (int [,] array)
+{
+int MinNumb = array [i,j];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (MinNumb >= array [i,j])
+        {
+            MinNumb = array [i,j];
+        }
+    }
+    
+}
+
+    int[,] Newarray = new int[GetLength(0) - 1, array.GetLength(1) - 1];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == MinNumb)
+            
+    
+        }
+    }   
+}
+}
+
+void PrintMatrix (int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr [i,j] + " ");     
+        }
+        Console.WriteLine();
+    }
+}
+
+
+const int ROWSCOUNT = 3;
+const int COLUNSCOUNT = 3;       
+const int lEFTRANGE = 0;
+const int RIGHTRANGE = 10;
+
+
+int [,] RandMatrix = RandomTwoDimensionalArray(ROWSCOUNT, COLUNSCOUNT, lEFTRANGE, RIGHTRANGE);
+PrintMatrix(RandMatrix);
