bf5ad0f33eaa52043987f651edb4b1dd605b87b3
«Maksim <gerasimmaks@mail.ru>
2022-12-21 21:31:37 +0300

    задача решена

---
+++ Seminar 8.0/task 55/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+
+int[,] RandomTwoDimensionalArray(int rowsCount, int colunsCount, int leftRange, int rightRange)
+{
+    int[,] array = new int[rowsCount, colunsCount];
+     
+    Random rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return array;
+}
+
+
+void PrintMatrix (int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr [i,j] + " ");     
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+int[,] ReplaisementRowsvsColuns(int[,] matrix)
+{
+    int[,] NewMatrix = new int[matrix.GetLength(1), matrix.GetLength(0)];
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+           NewMatrix [j,i] = matrix [i,j]; 
+        }
+    }
+    return NewMatrix;
+}
+
+
+
+
+
+const int ROWSCOUNT = 2;
+const int COLUNSCOUNT = 4;   
+const int lEFTRANGE = 1;
+const int RIGHTRANGE = 9;
+
+
+int [,] RandMatrix = RandomTwoDimensionalArray(ROWSCOUNT, COLUNSCOUNT, lEFTRANGE, RIGHTRANGE);
+PrintMatrix(RandMatrix);
+Console.WriteLine();
+int [,] ReplaiMatr = ReplaisementRowsvsColuns (RandMatrix);
+PrintMatrix(ReplaiMatr);
+
+
+
+
+
+
