3d23b368a671386de21e3534b8787f10c0b83fa5
«Maksim <gerasimmaks@mail.ru>
2022-12-22 20:32:21 +0300

    задача решена

---
+++ Seminar 8.0/homework/Task 58/Program.cs
@@ -0,0 +1,82 @@
+﻿//  Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, заданы 2 массива:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// и
+// 1 5 8 5
+// 4 9 4 2
+// 7 2 2 6
+// 2 3 4 7
+// Их произведение будет равно следующему массиву:
+// 1 20 56 10
+// 20 81 8 6
+// 56 8 4 24
+// 10 6 24 49
+
+
+int[,] RandomTwoDimensionalArray(int rowsCount, int colunsCount, int leftRange, int rightRange)
+{
+    int[,] array = new int[rowsCount, colunsCount];
+     
+    Random rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix (int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr [i,j] + " ");     
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] MultiplicetionMatrix (int[,] matr, int[,] matr2)
+{
+    int[,] Newarray = new int[matr.GetLength(0), matr.GetLength(1)];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Newarray[i, j] = matr[i, j] * matr2[i, j];
+        }
+    }
+    return Newarray;
+}
+
+
+const int ROWSCOUNT = 4;
+const int COLUNSCOUNT = 4;       
+const int lEFTRANGE = 1;
+const int RIGHTRANGE = 9;
+
+int [,] RandMatrix = RandomTwoDimensionalArray(ROWSCOUNT, COLUNSCOUNT, lEFTRANGE, RIGHTRANGE);
+PrintMatrix(RandMatrix);
+Console.WriteLine("");
+int [,] RandMatrix2 = RandomTwoDimensionalArray(ROWSCOUNT, COLUNSCOUNT, lEFTRANGE, RIGHTRANGE);
+PrintMatrix(RandMatrix2);
+Console.WriteLine("");
+Console.WriteLine("произведение двух матриц:");
+int [,] NewMatr = MultiplicetionMatrix (RandMatrix, RandMatrix2);
+PrintMatrix(NewMatr);
+
+
+
+
+
+
+
+
