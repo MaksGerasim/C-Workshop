7fed34a001a8681792ba835f9026571f78bd4e74
«Maksim <gerasimmaks@mail.ru>
2022-12-26 20:45:57 +0300

    задача решена

---
+++ Seminar 9.0/Task 65/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+int ConsoleEnterData (string enter)   
+{
+    Console.WriteLine(enter);
+    int meaning = Convert.ToInt32(Console.ReadLine());
+    return meaning;
+}
+
+
+void ConclusionInerval(int NumMin, int NumMax)
+{
+        Console.Write($"{NumMin}, ");
+        NumMin++;
+
+    if (NumMin < NumMax+1)
+    {
+        ConclusionInerval(NumMin, NumMax);
+    }
+}
+
+int Number = ConsoleEnterData ("введите первое число");
+int Number2 = ConsoleEnterData ("введите второе число");
+
+ConclusionInerval (Number, Number2);
